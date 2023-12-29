1e2838d9edc255a1e705aa7f7903683350a983c6
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-12-29 11:20:16 +0000

    feat: adding server.js example

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
@@ -8,15 +10,25 @@ const port = 3000;
 app.use(bodyParser.json());
 
 // Route to handle Apple's server notifications
-app.post("/apple-notifications", (req, res) => {
-    // Handle the transaction notification data sent by Apple here
-    console.log("Received Apple transaction notification:", req.body);
+app.post('/apple-notifications', (req, res) => {
+  // Handle the transaction notification data sent by Apple here
+  console.log('Received Apple transaction notification:', req.body);
 
-    // Respond with a 200 OK to acknowledge receipt of the notification
-    res.status(200).end();
+  // Respond with a 200 OK to acknowledge receipt of the notification
+  res.status(200).end();
 });
+
+const options = {
+  key: fs.readFileSync('localhost-key.pem'),
+  cert: fs.readFileSync('localhost.pem')
+};
+
+// Create HTTPS server
+const server = https.createServer(options, app);
 
 // Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`Server is running on https://localhost:${port}`);
 });
+
+//ssh -R 80:localhost:3000 localhost.run
